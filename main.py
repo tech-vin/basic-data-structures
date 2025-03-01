febc75ab5720561ae273776c87b6e468c2e01a0e
Singh <vineetkumar.singh@dellteam.com>
2025-03-02 01:32:13 +0530

    new function to check palindrome added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def sumOfDigits(num):
     result = 0
     while num > 0:
         rem = num % 10
-        result = result * 10 + rem
+        result = result + rem
         num = num // 10
     return result
 
@@ -15,6 +15,25 @@ def countDigits(num):
         num = num // 10
     return count
 
+def reverseDigits(num):
+    result = 0
+    while num > 0:
+        rem  = num % 10
+        result = result * 10 + rem
+        num = num // 10
+    return result
+
+def isPalindrome(num):
+    cpy = num
+    op = reverseDigits(num)
+    if op == cpy:
+        return True
+    return False
+
 nums = 12321
+print(isPalindrome(123))
+print(isPalindrome(121))
+print(isPalindrome(nums))
+print(reverseDigits(nums))
 print(countDigits(nums))
 print(sumOfDigits(nums))
